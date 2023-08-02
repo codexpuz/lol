1bee4c3d7cb3d5c19150ec54d94a865a6ddd7a45
codexpuz <123955458+codexpuz@users.noreply.github.com>
2023-08-02 10:38:53 -0700

    Update and rename power.ps1 to install.ps1

--- power.ps1
+++
@@ -1,8 +0,0 @@
-$pythonUrl = "https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe"
-$pythonInstaller = "$env:TEMP\python-3.9.7-amd64.exe"
-Invoke-WebRequest -Uri $pythonUrl -OutFile $pythonInstaller
-Start-Process -Wait -FilePath $pythonInstaller -ArgumentList "/quiet", "PrependPath=1" -Verb RunAs
-Remove-Item -Path $pythonInstaller -Force
-$pythonBinFolder = (Get-Command python).Source.Substring(0, (Get-Command python).Source.LastIndexOf('\'))
-$env:Path += ";$pythonBinFolder"
-Start-Process -FilePath "cmd.exe" -ArgumentList "/C pip install slowloris" -Verb RunAs
